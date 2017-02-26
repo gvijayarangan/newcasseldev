c61374f6ffd0a7a3c6c27a0770b673d43df1b7b0
Padmanabhan Soundararajan <psoundararajan@unomaha.edu>
2017-02-26 16:55:26 -0600

    Code cleansing for Apartments and supply pages

--- app/Http/Controllers/ApartmentsController.php
+++ app/Http/Controllers/ApartmentsController.php
@@ -6,14 +6,13 @@ use Illuminate\Http\Request;
 use App\Apartment;
 use App\Center;
 
-//adding comment for demo1
 class ApartmentsController extends Controller
 {
 
     public function index()
     {
         $createapts = Apartment::all();
-        foreach ($createapts as $apts) {//dd(Center::findOrFail(7)->cntr_name);
+        foreach ($createapts as $apts) {
             $apts->centerName = Center::findOrFail($apts->cntr_id)->cntr_name;
         }
         return view('CreateApt.index',compact('createapts'));
@@ -83,7 +82,7 @@ class ApartmentsController extends Controller
         $CreateApt->apt_floornumber = $request->apt_floornumber;
         $CreateApt->apt_number = $request->apt_number;
         $CreateApt->apt_comments = $request->apt_comments;
-        $CreateApt->cntr_id = $request->cntr_id; //commented to avoid FK from Center table
+        $CreateApt->cntr_id = $request->cntr_id; 
         $CreateApt->save();
         return redirect('apartment');
     }
